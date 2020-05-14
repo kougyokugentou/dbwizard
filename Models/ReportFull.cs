f1bb12e590ad82a8b93a55df8ed1a1be5f866c60
kougyokugentou <41278462+kougyokugentou@users.noreply.github.com>
2020-05-14 14:33:29 -0700

    Add full report data model.

---
+++ Models/ReportFull.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBWizard.Models
+{
+    class ReportFull
+    {
+        public int id { get; set; }
+        public string student_id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DOB { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public string ProgramName { get; set; }
+        public string SchoolName { get; set; }
+        public string GradeLevel { get; set; }
+        public string ParentFirstName { get; set; }
+        public string ParentLastName { get; set; }
+        public string ParentPhoneNumber { get; set; }
+        public string ParentEmailAddress { get; set; }
+    }
+}
